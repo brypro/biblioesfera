e42e8f06706d10b047221f4b2cfa15190f91403d
sistemas <sistemas@SPO_LAB50925.inacap.cl>
2024-05-27 19:54:03 -0400

    cambios temas

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+
 function openRegisterModal() {
   document.getElementById("register_modal").showModal();
 }
@@ -8,4 +9,26 @@ function closeRegisterModal() {
 
 function openLoginModal() {
   login_modal.showModal();
+}
+
+tailwind.config = {
+theme: {
+  colors: {
+    'light': '#1A1A1A',
+    'dark': '#1A1A1A',
+  },
+  fontFamily: {
+    sans: ['Graphik', 'sans-serif'],
+    serif: ['Merriweather', 'serif'],
+  },
+  extend: {
+    spacing: {
+      '8xl': '96rem',
+      '9xl': '128rem',
+    },
+    borderRadius: {
+      '4xl': '2rem',
+    }
+  }
+},
 }
