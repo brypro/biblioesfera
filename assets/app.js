5438dc74a22190ea680986df4d6044b48fec666e
bperalta <bperalta@gmail.com>
2024-05-26 20:56:33 -0400

    refactor: Add login and register modals for user authentication

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+function openRegisterModal() {
+  document.getElementById("register_modal").showModal();
+}
+
+function closeRegisterModal() {
+  document.getElementById("register_modal").close();
+}
+
+function openLoginModal() {
+  login_modal.showModal();
+}
