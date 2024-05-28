85b3580e0df236b344dbb5ef0c06f3208fc80dc1
sistemas <sistemas@SPO_LAB50613.inacap.cl>
2024-05-27 21:05:08 -0400

    botones cierre modales

--- assets/app.js
+++ assets/app.js
@@ -10,25 +10,3 @@ function closeRegisterModal() {
 function openLoginModal() {
   login_modal.showModal();
 }
-
-tailwind.config = {
-theme: {
-  colors: {
-    'light': '#1A1A1A',
-    'dark': '#1A1A1A',
-  },
-  fontFamily: {
-    sans: ['Graphik', 'sans-serif'],
-    serif: ['Merriweather', 'serif'],
-  },
-  extend: {
-    spacing: {
-      '8xl': '96rem',
-      '9xl': '128rem',
-    },
-    borderRadius: {
-      '4xl': '2rem',
-    }
-  }
-},
-}
